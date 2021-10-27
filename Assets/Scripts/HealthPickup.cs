bbb7f30eb5be00a139a424bec474559c0a71d0bf
J4rpp1 <jaka765@gmail.com>
2021-10-27 13:18:51 +0300

    bug fixes

--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -6,24 +6,29 @@ public class HealthPickup : MonoBehaviour
 {
     PlayerHealth playerHealth;
     public float healthBonus = 15f;
+    public bool canCollect;
    
 
     private void Awake()
     {
+        canCollect = true;
         playerHealth = FindObjectOfType<PlayerHealth>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (playerHealth.currentHealth < playerHealth.maxHealth)
+        if (playerHealth.currentHealth < playerHealth.maxHealth && canCollect)
         {
+            canCollect = false;
             Destroy(gameObject);
             playerHealth.currentHealth = playerHealth.currentHealth + healthBonus;
-
+            
             //Score.instance.displayScore += 10;
             Score.instance.AddPoint();
             Score.instance.AddCollected();
             PlayerHealth.instance.PickupSound();
+            //Debug.Log("kerätty");
+            
 
         }
     }
